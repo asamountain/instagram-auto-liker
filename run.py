3624d51ef8726d0307ed844aea014579f2092b82
SJ <sjisyours@gmail.com>
2019-07-25 00:56:14 +0800

    switching update

--- run.py
+++ run.py
@@ -16,6 +16,10 @@ import pyglet as pg
 import selenium
 import sys
 import random
+import datetime
+
+from tinydb import TinyDB, Query
+from tinydb import where
 
 from selenium import webdriver
 from selenium.webdriver import Remote
@@ -33,48 +37,81 @@ from traceback import print_exc
 # waiting function added
 from selenium.webdriver.support.ui import WebDriverWait
 
+def addAccount(db, userDataDir) :
+                print('Will add new account info')
+                id = input('ID > ')
+                passd = input('PW > ')
+                folder = input('Cookie Folder Name > ')
+                followerCount = int(input('How many follow today? > '))
+                userDataDir = "user-data-dir=" + folder
+
+                db.insert({'id':id,'passd':passd,'folder':folder,'followerCount':followerCount})
+                return db, userDataDir
+
 
 def main():
     print('Instagram Auto Liker Activating...')
 
-    # Encode init
-    print('Encode init')
-
     # Chrome init
     try:
-        # Chrome options
+        folder=''
+        userDataDir = ''
+        db = TinyDB('account.json', sort_keys=False, separators=(',',': '))
+
+        
+        if len(db.all()) == 0 :
+            print('None of account info detected')
+            addAccount(db,userDataDir)
+
+        else : 
+            
+            switch = int(input('What Would you like to do? \n 1. login \n 2. add account \n 3. remove accounts \n > '))
+
+            if switch == 1 : 
+               print(db.all())
+               loginSelection = int(input('Which account would you login? > '))
+               selectedAccount = db.get(doc_id=loginSelection)
+               print(selectedAccount)
+               id = selectedAccount['id']
+               passd = selectedAccount['passd']
+               folder = selectedAccount['folder']
+               followerCount = selectedAccount['followerCount']
+               print (id, passd, folder, followerCount)
+
+            elif switch == 2:
+                addAccount(db,userDataDir)
+
+            elif switch == 3 :
+                removal = input('Would you like to remove all login info? y or n > ')
+                if removal == 'y' : 
+                    db.purge()
+                    db.all()
+                else :
+                    pass
+
+
+        # To put a Cookie folder 
         chrome_options = webdriver.ChromeOptions()
         prefs = {"profile.default_content_setting_values.notifications" : 2}
+
+        userDataDir = "user-data-dir=" + folder
+        chrome_options.add_argument(userDataDir)
         chrome_options.add_experimental_option("prefs",prefs)
-
-
         chrome_options.add_argument("--start-maximized")
-
-
-        print ('----------------------------------------')
-        print ('[login id, identified]')
-        print ('id : %s' % id )
-        print ('pw : %s' % passd )
-        print ('----------------------------------------')
-        print (chrome_options._arguments)
-
-        # Creating driver
         driver = webdriver.Chrome(chrome_options=chrome_options)
-        driver.implicitly_wait(5)
-        # 1st driver opens the url
+        driver.implicitly_wait(3)
         url = 'https://www.instagram.com/accounts/login/?source=auth_switcher'
         driver.get(url)
 
 
-
-        print('#1 chrome to open')
+        hashtag = [u"#유기묘",u"#사지마세요입양하세요",u"#사지말고입양하세요",u"#환경보호",u"#플라스틱",u"#플라스틱프리챌린지",u"#일회용품줄이기",u"#요가원",u"#일회용품줄이기",u"#제로웨이스트",u'#요가여행',u"#요가프로필",u"#yogabarn",u"#radiantlyyoga",u"#ecofriendly",u"#plasticfree",u"#플라스틱프리",u"#발리요가",u"#발리요가",u"#명상",u"#stillness",u"#mediation",u"#요가반",u"#ecofriendly",u"#ecolife",u"#에코라이프",u"#플라스틱줄이기"] 
+        # hashtag = [u"#아깽이",u"#아깽이그램",u"#냥이",u"#구조냥",u"#catstagram",u"#kitten",u"#아깽이",u'#구조고양이',u"#고양이",u"#집사",u"#냥스타그램",u"#냥이",u"#고양이그램"]
+        # hashtag = [u"#긍정부자",u"#여행자",u"#배낭여행",u"#동물구조",u"#생산성",u"#독서그램",u"#독서스타그램"]
 
         time.sleep(1)
 
-
         # Check whether it's login page or not
         try :
-
             print('#3 trying to user info for login')
             username = driver.find_element_by_name('username')
             username.send_keys(id)
@@ -106,30 +143,31 @@ def main():
             # scrolling & liked counts init in Homefeed
             c = 0
             print('home like %s counting init' % c)
-                
             for i in range(0, 200):
-                
                 like = driver.find_elements_by_css_selector("span.fr66n button span.glyphsSpriteHeart__outline__24__grey_9")
-             
                 print ('Like Hearts : %s ' % like)
-                home_feed_liker(like, driver, c)
-                time.sleep(0.5)
-                scrolling(driver,c)
+                if len(like)==0 : 
+                    print('There is no more hearts likable')
+                    break
+                else : 
+                    home_feed_liker(like, driver, c)
+                    time.sleep(0.5)
+                    scrolling(driver,c)
 
 
 
         # hashtag liker
         elif user_input =='t' :
             print (' %s key pressed, Tag Liker Activated' % user_input)
-
+            print ('tags : [ %s ] ' % hashtag )
             f = 0
             i = 0
             while ( i < len(hashtag) ):
-                hashtag_liker(driver, hashtag, i, f)
+                hashtag_liker(driver, hashtag, i, f, followerCount)
                 i = i + 1
 
         else:
-            user_input = input("t or h?")
+            user_input = input("'H'omefeed / on 'T'ag feed > ")
             print (': %s' % user_input)   
 
 
@@ -145,7 +183,7 @@ def main():
 
 
 
-def hashtag_liker(driver, hashtag, i,f):
+def hashtag_liker(driver, hashtag, i,f, followerCount):
         lenList = len(hashtag)-1
         ranNum = random.randint(0,lenList)
         print ('%s th hashtag in hashtag list, will be chosen ' % ranNum)
@@ -161,10 +199,14 @@ def hashtag_liker(driver, hashtag, i,f):
         print ('#7 try to click target hastag list')
         target_tag_elem_list = driver.find_elements_by_css_selector('a.yCE8d  ')
         print ('target_tag_elem_list length :', len(target_tag_elem_list))
-        target_tag_elem = target_tag_elem_list[0]
-        print ('target_tag_elem : ' , target_tag_elem)
-        target_tag_elem.click() # first listed hashtag click!
-
+        try : 
+            target_tag_elem = target_tag_elem_list[0]
+            print ('target_tag_elem : ' , target_tag_elem)
+            target_tag_elem.click() # first listed hashtag click!
+            # hastag list isn't clickable, then...
+        except : 
+            pass
+       
 
 
         # Click Most recent img
@@ -176,9 +218,9 @@ def hashtag_liker(driver, hashtag, i,f):
 
         most_recent_img.click()
 
-        clicked_img = driver.find_element_by_css_selector('img.FFVAD')
-        img_alt = clicked_img.get_attribute('alt')
-        print ('img alt : %s ' % img_alt)
+        # clicked_img = driver.find_element_by_css_selector('img.FFVAD')
+        # img_alt = clicked_img.get_attribute('alt')
+        # print ('img alt : %s ' % img_alt)
         
         nsee_count = 0
         for i in range(0,500):
@@ -202,7 +244,7 @@ def hashtag_liker(driver, hashtag, i,f):
                 if '_8A5w5' in elem_class :
                     pass # 'already following' button has the '_8a5w5' class name
 
-                elif f < 30 :   # 30 people following possible in a term only!
+                elif f < followerCount :   # 30 people following possible in a term only!
         
                     # print ('follow click unactivated'    )
                     follow.click()
@@ -244,11 +286,11 @@ def home_feed_liker(like, driver,c ):
 
         for like_link in like:
             try:
-                print(' ')
+                print('How many likes in this page > %s ' % len(like) )
                 print('like_link : %s ' % like_link)
-                print(' ')
                 like_link.location_once_scrolled_into_view
                 like_link.click()
+                time.sleep(0.5)
                 c = c + 1
                 print('%s th liked! ' % c)
 
